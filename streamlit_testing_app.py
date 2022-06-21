571a8768346d17d7aff629fa8c3e2d4d2a5c8568
Francisco Soster <kowal.flecha@gmail.com>
2022-06-21 12:19:18 -0300

    add st.file_uploader

--- streamlit_testing_app.py
+++ streamlit_testing_app.py
@@ -3,3 +3,15 @@ import pandas as pd
 import streamlit as st
 
 st.title("Streamlit Testing App")
+
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
